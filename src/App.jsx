1dcfb75484e5f353449dd4e88cc43dd8a84d36c0
Mouad-MCH <mouadcharadi021@gmail.com>
2026-04-20 17:57:24 +0100

    setup the project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Offers from './pages/Offers'
+
+const App = () => {
+  return (
+    <Offers/>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <h1>CreditConcierge</h1>
+      <ul className="nav_list">
+        <li>
+            <a href='#'>compare</a>
+        </li>
+        <li>
+            <a href="#">offers</a>
+        </li>
+      </ul>
+
+      <div className="icons">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
